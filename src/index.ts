e5fe040d278fe6c8cb68f0742ac56ebb7700db4d
Brett Horan <brett@thirstcreative.com.au>
2023-04-27 11:59:03 +1000

    Staging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
-import { greetUser } from '$utils/greet';
+//import LocomotiveScroll from 'locomotive-scroll';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+//import Splitting from 'splitting';
+
+/* Prevent scroling when menu is open */
+
+// const navButton = document.querySelector('.w-nav-button');
+
+// navButton.addEventListener('click', function () {
+//   if (navButton.classList.contains('w--open')) {
+//     document.body.style.overflow = 'auto';
+//   } else {
+//     document.body.style.overflow = 'hidden';
+//   }
+// });
+
+//Splitting();
+
+Webflow.push(function () {
+  // check is the CMS editor is there
+  if (Webflow.env('editor') !== true) {
+    // code here
+  }
 });
+
+const navIcon = document.querySelector('.w-nav-button');
+const navClose = document.querySelector('.kestral-hamburger');
+const navLinks = document.querySelectorAll('.w-nav-link');
+
+navIcon.addEventListener('click', function () {
+  document.body.style.overflow = 'hidden';
+});
+
+navClose.addEventListener('click', function () {
+  document.body.style.overflow = '';
+});
+
+navLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    document.body.style.overflow = '';
+  });
+});
+
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector('[data-scroll-container]'),
+//   smooth: true,
+// });
